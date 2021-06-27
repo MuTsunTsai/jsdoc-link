3ff28edf0e673125ef91666e08e576005a12d725
MuTsunTsai <don.m.t.tsai@gmail.com>
2021-06-27 17:51:23 +0800

    Improve performance

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,9 @@
 # Change Log
 
-All notable changes to the "jsdoc-live" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
+## 0.1.0
 
 - Initial release
+
+## 0.1.1
+
+- Improve performance
--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@ import { throttle } from 'lodash';
 
 const THROTTLE_DELAY = 800
 const hoverEnableDecorationType = vscode.window.createTextEditorDecorationType({
+	// `display:none;` will cause trouble with hover message
 	textDecoration: 'none; display:inline-block; width:0; height:0; overflow:hidden;',
 });
 const hiddenDecorationType = vscode.window.createTextEditorDecorationType({
@@ -15,8 +16,6 @@ const linkPattern = /(\{\@link\s+)([^}\s]+)(?:\s+([^}]+?))?\}/gs;
 export function activate(context: vscode.ExtensionContext) {
 	console.log('jsdoc-link activated');
 
-	process();
-
 	const throttledProcess = throttle(() => process(), THROTTLE_DELAY);
 
 	vscode.window.onDidChangeActiveTextEditor(throttledProcess, null, context.subscriptions);
@@ -25,12 +24,21 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.workspace.onDidChangeTextDocument(event => {
 		let editor = vscode.window.activeTextEditor;
 		if(event.document == editor?.document) throttledProcess();
-	}, null, context.subscriptions)
+	}, null, context.subscriptions);
+
+	throttledProcess();
 }
 
-let hiddenDecorator: vscode.DecorationOptions[];
-let hoverDecorator: vscode.DecorationOptions[];
+interface DecoratorSet {
+	start: vscode.Position;
+	end: vscode.Position;
+	options: vscode.DecorationOptions[];
+}
+
+let decoratorSets: DecoratorSet[];
 let linkColor: vscode.ThemeColor;
+
+let textCache: string;
 
 function process() {
 	const editor = vscode.window.activeTextEditor;
@@ -41,61 +49,81 @@ function process() {
 	const lang = document.languageId;
 	if(lang != "javascript" && lang != "typescript") return;
 
-	// initialize
-	hiddenDecorator = [];
-	hoverDecorator = [];
-	linkColor = new vscode.ThemeColor('textLink.foreground');
-
-	let selection = editor.selection;
+	// Re-scan document only if text has changed
 	let text = document.getText();
-	let match: RegExpExecArray | null;
-	while(match = commentPattern.exec(text)) {
-		let pos = match.index + 3;
-		processLink(pos, match[1], document, selection);
+	if(textCache != text) {
+		textCache = text;
+		decoratorSets = [];
+		linkColor = new vscode.ThemeColor('textLink.foreground');
+		let match: RegExpExecArray | null;
+		while(match = commentPattern.exec(text)) {
+			let pos = match.index + 3;
+			processLink(pos, match[1], document);
+		}
 	}
 
-	editor.setDecorations(hoverEnableDecorationType, hoverDecorator);
-	editor.setDecorations(hiddenDecorationType, hiddenDecorator);
+	// Exclude selected ones
+	let selection = editor.selection;
+	let sets = decoratorSets.filter(d =>
+		(selection.start.line > d.start.line || d.start.line > selection.end.line) &&
+		(selection.start.line > d.end.line || d.start.line > selection.end.line)
+	);
+
+	// Grouping
+	let hover: vscode.DecorationOptions[] = [];
+	let hidden: vscode.DecorationOptions[] = [];
+	for(let set of sets) for(let option of set.options) {
+		if(option.renderOptions) hover.push(option);
+		else hidden.push(option);
+	}
+
+	// Set decorations
+	editor.setDecorations(hoverEnableDecorationType, hover);
+	editor.setDecorations(hiddenDecorationType, hidden);
 }
 
-function processLink(pos: number, text: string, document: vscode.TextDocument, selection: vscode.Selection) {
+function processLink(pos: number, text: string, document: vscode.TextDocument) {
 	let match: RegExpExecArray | null;
 	while(match = linkPattern.exec(text)) {
 		let s = pos + match.index;
 		let start = document.positionAt(s);
 		let end = document.positionAt(s + match[0].length);
-		if((selection.start.line <= start.line && start.line <= selection.end.line) ||
-			(selection.start.line <= end.line && end.line <= selection.end.line)) continue;
 
 		if(!match[3]) {
 			// when alt text is not used, a simple replacement will do
-			hoverDecorator.push({
-				range: new vscode.Range(start, end),
-				renderOptions: {
-					after: {
-						color: linkColor,
-						contentText: match[2],
-						fontStyle: 'normal'
+			decoratorSets.push({
+				start, end,
+				options: [{
+					range: new vscode.Range(start, end),
+					renderOptions: {
+						after: {
+							color: linkColor,
+							contentText: match[2],
+							fontStyle: 'normal'
+						}
 					}
-				}
-			});
+				}]
+			})
 		} else {
 			// extra care is necessary to keep the hover message
 			let p1 = document.positionAt(s + match[1].length);
 			let p2 = document.positionAt(s + match[1].length + match[2].length);
-			hiddenDecorator.push(
-				{ range: new vscode.Range(start, p1) },
-				{ range: new vscode.Range(p2, end) }
-			);
-			hoverDecorator.push({
-				range: new vscode.Range(p1, p2),
-				renderOptions: {
-					after: {
-						color: linkColor,
-						contentText: match[3],
-						fontStyle: 'normal'
+			decoratorSets.push({
+				start, end,
+				options: [
+					{ range: new vscode.Range(start, p1) },
+					{ range: new vscode.Range(p2, end) },
+					{
+						range: new vscode.Range(p1, p2),
+						renderOptions: {
+							after: {
+								color: linkColor,
+								contentText: match[3],
+								fontStyle: 'normal'
+							}
+						}
 					}
-				}
+				]
 			});
 		}
 	}
