e4e627ed22a35d83094ec894ceffd95f022cf0d2
MuTsunTsai <don.m.t.tsai@gmail.com>
2021-06-29 20:12:35 +0800

    Reduce bundle size

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,69 @@
-import * as vscode from 'vscode';
-import { throttle } from 'lodash';
+import * as vs from 'vscode';
 
-const THROTTLE_DELAY = 800
-const hoverEnableDecorationType = vscode.window.createTextEditorDecorationType({
+const THROTTLE_DELAY = 800;
+const hoverEnableDecorationType = vs.window.createTextEditorDecorationType({
 	// `display:none;` will cause trouble with hover message
 	textDecoration: 'none; display:inline-block; width:0; height:0; overflow:hidden;',
 });
-const hiddenDecorationType = vscode.window.createTextEditorDecorationType({
+const hiddenDecorationType = vs.window.createTextEditorDecorationType({
 	textDecoration: 'none; display:none;',
 });
 
 const commentPattern = /\/\*\*(.+?)\*\//gs;
-const linkPattern = /(\{\@link\s+)([^|}\s]+)(?:[|\s]\s*([^}\s][^}]*)|\s+)?\}/gs;
+const linkPattern = /(\{@link\s+)([^|}\s]+)(?:[|\s]\s*([^}\s][^}]*)|\s+)?\}/gs;
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vs.ExtensionContext): void {
 	console.log('jsdoc-link activated');
 
-	const throttledProcess = throttle(() => process(), THROTTLE_DELAY);
+	const throttledProcess = throttle(process, THROTTLE_DELAY);
 
-	vscode.window.onDidChangeActiveTextEditor(throttledProcess, null, context.subscriptions);
-	vscode.window.onDidChangeTextEditorSelection(throttledProcess, null, context.subscriptions);
+	vs.window.onDidChangeActiveTextEditor(throttledProcess, null, context.subscriptions);
+	vs.window.onDidChangeTextEditorSelection(throttledProcess, null, context.subscriptions);
 
-	vscode.workspace.onDidChangeTextDocument(event => {
-		let editor = vscode.window.activeTextEditor;
+	vs.workspace.onDidChangeTextDocument(event => {
+		const editor = vs.window.activeTextEditor;
 		if(event.document == editor?.document) throttledProcess();
 	}, null, context.subscriptions);
 
 	throttledProcess();
 }
 
+function throttle(func: () => void, wait: number) {
+	let last = 0;
+	let pending = false;
+	function throttled() {
+		if(pending) return;
+		const diff = Date.now() - last;
+		if(diff > wait) {
+			func();
+			console.log("throttle");
+			last += diff;
+		} else {
+			pending = true;
+			setTimeout(() => {
+				pending = false;
+				throttled();
+			}, wait - diff);
+		}
+	}
+	return throttled;
+}
+
 interface DecoratorSet {
-	start: vscode.Position;
-	end: vscode.Position;
-	options: vscode.DecorationOptions[];
+	start: vs.Position;
+	end: vs.Position;
+	options: vs.DecorationOptions[];
 }
 
 let decoratorSets: DecoratorSet[];
-let linkColor: vscode.ThemeColor;
+let linkColor: vs.ThemeColor;
 
 let textCache: string;
 
 const supportedLang = ['javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue'];
 
-function process() {
-	const editor = vscode.window.activeTextEditor;
+function process(): void {
+	const editor = vs.window.activeTextEditor;
 	const document = editor?.document;
 	if(!editor || !document) return;
 
@@ -52,31 +72,33 @@ function process() {
 	if(!supportedLang.includes(lang)) return;
 
 	// Re-scan document only if text has changed
-	let text = document.getText();
+	const text = document.getText();
 	if(textCache != text) {
 		textCache = text;
 		decoratorSets = [];
-		linkColor = new vscode.ThemeColor('textLink.foreground');
+		linkColor = new vs.ThemeColor('textLink.foreground');
 		let match: RegExpExecArray | null;
 		while(match = commentPattern.exec(text)) {
-			let pos = match.index + 3;
+			const pos = match.index + 3;
 			processLink(pos, match[1], document);
 		}
 	}
 
 	// Exclude selected ones
-	let selection = editor.selection;
-	let sets = decoratorSets.filter(d =>
+	const selection = editor.selection;
+	const sets = decoratorSets.filter(d =>
 		(selection.start.line > d.start.line || d.start.line > selection.end.line) &&
 		(selection.start.line > d.end.line || d.start.line > selection.end.line)
 	);
 
 	// Grouping
-	let hover: vscode.DecorationOptions[] = [];
-	let hidden: vscode.DecorationOptions[] = [];
-	for(let set of sets) for(let option of set.options) {
-		if(option.renderOptions) hover.push(option);
-		else hidden.push(option);
+	const hover: vs.DecorationOptions[] = [];
+	const hidden: vs.DecorationOptions[] = [];
+	for(const set of sets) {
+		for(const option of set.options) {
+			if(option.renderOptions) hover.push(option);
+			else hidden.push(option);
+		}
 	}
 
 	// Set decorations
@@ -84,19 +106,19 @@ function process() {
 	editor.setDecorations(hiddenDecorationType, hidden);
 }
 
-function processLink(pos: number, text: string, document: vscode.TextDocument) {
+function processLink(pos: number, text: string, document: vs.TextDocument) {
 	let match: RegExpExecArray | null;
 	while(match = linkPattern.exec(text)) {
-		let s = pos + match.index;
-		let start = document.positionAt(s);
-		let end = document.positionAt(s + match[0].length);
-		let alt = match[3]?.trim();
+		const s = pos + match.index;
+		const start = document.positionAt(s);
+		const end = document.positionAt(s + match[0].length);
+		const alt = match[3]?.trim();
 		if(!alt) {
 			// when alt text is not used, a simple replacement will do
 			decoratorSets.push({
 				start, end,
 				options: [{
-					range: new vscode.Range(start, end),
+					range: new vs.Range(start, end),
 					renderOptions: {
 						after: {
 							color: linkColor,
@@ -105,18 +127,18 @@ function processLink(pos: number, text: string, document: vscode.TextDocument) {
 						}
 					}
 				}]
-			})
+			});
 		} else {
 			// extra care is necessary to keep the hover message
-			let p1 = document.positionAt(s + match[1].length);
-			let p2 = document.positionAt(s + match[1].length + match[2].length);
+			const p1 = document.positionAt(s + match[1].length);
+			const p2 = document.positionAt(s + match[1].length + match[2].length);
 			decoratorSets.push({
 				start, end,
 				options: [
-					{ range: new vscode.Range(start, p1) },
-					{ range: new vscode.Range(p2, end) },
+					{ range: new vs.Range(start, p1) },
+					{ range: new vs.Range(p2, end) },
 					{
-						range: new vscode.Range(p1, p2),
+						range: new vs.Range(p1, p2),
 						renderOptions: {
 							after: {
 								color: linkColor,
@@ -131,6 +153,6 @@ function processLink(pos: number, text: string, document: vscode.TextDocument) {
 	}
 }
 
-export function deactivate() {
+export function deactivate(): void {
 	console.log('jsdoc-link deactivated.');
 }
